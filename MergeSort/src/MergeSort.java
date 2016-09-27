9bc28165595c487170ea7b8784f280c45480d95a
Aishwarya Pratap Singh <pratapsingh.aishwarya@hotmail.com>
2016-09-27 15:02:08 -0700

    first commit

---
+++ MergeSort/src/MergeSort.java
@@ -0,0 +1,10 @@
+/**
+ * Created by aishpratap on 9/27/16.
+ */
+public class MergeSort {
+
+    public static void main(String args[]){
+
+    }
+
+}
---
+++ SelectionSort/src/SelectionSort.java
@@ -0,0 +1,55 @@
+import java.io.BufferedInputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Created by aishpratap on 9/27/16.
+ */
+public class SelectionSort {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(new BufferedInputStream(System.in));
+        int testCases = scanner.nextInt();
+
+        for (int tests = 0; tests < testCases; tests++){
+
+            System.out.println("To end the array of elements, enter '-1'");
+
+            ArrayList<Integer> unsortedArray = new ArrayList();
+
+            while (scanner.hasNextInt()){
+
+                int value = scanner.nextInt();
+                if (value<0){
+                    break;
+                }
+                unsortedArray.add(value);
+
+            }
+
+            unsortedArray = selectionSort(unsortedArray);
+            System.out.println(unsortedArray);
+        }
+    }
+
+    static ArrayList<Integer> selectionSort(ArrayList<Integer> unsorted){
+
+        for (int i = 0; i<unsorted.size(); i++){
+
+            int lowestIndex = i;
+
+            for (int j = i+1; j<unsorted.size(); j++){
+
+                if (unsorted.get(j)<unsorted.get(lowestIndex)){
+                    lowestIndex = j;
+                }
+            }
+
+            int temp = unsorted.get(i);
+            unsorted.set(i,unsorted.get(lowestIndex));
+            unsorted.set(lowestIndex, temp);
+        }
+
+        return unsorted;
+    }
+
+}
